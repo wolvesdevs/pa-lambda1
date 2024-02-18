72b3450ee6a008f3213ede765286ae8418acc4c5
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2024-02-18 19:53:18 +0900

    #03 ラムダなしで実装

--- src/Lambda1/Lambda1/Form1.cs
+++ src/Lambda1/Lambda1/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Lambda1
 {
     public partial class Form1 : Form
@@ -5,6 +7,24 @@ namespace Lambda1
         public Form1()
         {
             InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // #03 ƒ‰ƒ€ƒ_‚È‚µ‚ÅŽÀ‘•
+            var values = new string[] { "A", "BB", "CCC", "DDDD", "EEEEE" };
+            var result = new List<string>();
+
+            foreach (var value in values)
+            {
+                if (value.Length >= 3)
+                {
+                    result.Add(value);
+                }
+            }
+
+            Debug.WriteLine(string.Join(", ", result));
         }
     }
 }
